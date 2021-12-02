4605bc8a97c5298445e1d726595d1446fe0a0621
dnguye12 <duchuyng051@gmail.com>
2021-12-02 08:07:53 +0100

    Please

--- 2048.cpp
+++ 2048.cpp
@@ -1,24 +1,13 @@
-#include <SFML/Graphics.hpp>
+#include "modele.h"
+#include "modele.cpp"
+
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    openmenu();
+   joue();
+   return 0;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
 }
