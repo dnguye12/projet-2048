1bcb084e6442f2c46ee3c7f0a1ed063e13b3ae28
Nguyen Duc Huy <duchuyng051@gmail.com>
2022-02-09 10:24:36 +0100

    Update modele.h

--- modele.h
+++ modele.h
@@ -21,119 +21,115 @@ typedef vector<vector<int> > Plateau;
 map<string, int> score_list;
 
 
-/** g�n�re un Plateau de dimensions 4*4 ne contenant que des 0
+/** génère un Plateau de dimension 4*4 ne contenant que des 0
  *  @return un Plateau vide
  **/
-Plateau plateauVide();
+Plateau plateauVide(); /*implantée par Gloire LINVANI
 
 
-/** g�n�re deux nombres sur des cases al�atoires d'un Plateau vide
- *  @return un Plateau en d�but de jeu
+/** génère deux nombres sur des cases aléatoires d'un Plateau vide
+ *  @return un Plateau en début de jeu
  **/
-Plateau plateauInitial();
+Plateau plateauInitial(); /*implantée par Gloire LINVANI
 
 
-/** g�n�re al�atoirement un 2 ou un 4 avec des probabilit�s respectives de 9/10 et 1/10
+/** génè aléatoirement un 2 ou un 4 avec des probabilités respectives de 9/10 et 1/10
  *  @return 2 ou 4
  **/
-int tireDeuxOuQuatre();
+int tireDeuxOuQuatre(); /*implantée par Gloire LINVANI
 
 
-/** d�place les tuiles d'un Plateau vers la gauche et les combine si possible
+/** déplace les tuiles d'un Plateau vers la gauche et les combine si possible
  *  @param plateau le Plateau
- *  @return le Plateau une fois d�plac� vers la gauche
+ *  @return le Plateau une fois déplacé vers la gauche
  **/
-Plateau deplacementGauche(Plateau plateau);
+Plateau deplacementGauche(Plateau plateau); /*implantée par Huy NGUYEN
 
 
-/** d�place les tuiles d'un Plateau vers la droite et les combine si possible
+/** déplace les tuiles d'un Plateau vers la droite et les combine si possible
  *  @param plateau le Plateau
- *  @return le Plateau une fois d�plac� vers la droite
+ *  @return le Plateau une fois déplacé vers la droite
  **/
-Plateau deplacementDroite(Plateau plateau);
+Plateau deplacementDroite(Plateau plateau); /*implantée par Huy NGUYEN
 
 
-/** d�place les tuiles d'un Plateau vers le haut et les combine si possible
+/** déplace les tuiles d'un Plateau vers le haut et les combine si possible
  *  @param plateau le Plateau
- *  @return le Plateau une fois d�plac� vers le haut
+ *  @return le Plateau une fois déplacé vers le haut
  **/
-Plateau deplacementHaut(Plateau plateau);
+Plateau deplacementHaut(Plateau plateau); /*implantée par Huy NGUYEN
 
 
-/** d�place les tuiles d'un Plateau vers le bas et les combine si possible
+/** déplace les tuiles d'un Plateau vers le bas et les combine si possible
  *  @param plateau le Plateau
- *  @return le Plateau une fois d�plac� vers le bas
+ *  @return le Plateau une fois déplacé vers le bas
  **/
-Plateau deplacementBas(Plateau plateau);
+Plateau deplacementBas(Plateau plateau); /*implantée par Huy NGUYEN
 
 
-/** d�place les tuiles d'un Plateau dans la direction donn�e et g�n�re une nouvelle tuile si le d�placement est valide
+/** déplace les tuiles d'un Plateau dans la direction donnée et génère une nouvelle tuile si le deplacement est valide
  *  @param plateau le Plateau
  *  @param direction la direction
- *  @return le Plateau d�plac� dans la direction
+ *  @return le Plateau déplacé dans la direction
  **/
-Plateau deplacement(Plateau plateau, int direction);
+Plateau deplacement(Plateau plateau, int direction); /*implantée par Huy NGUYEN
 
 
 /** affiche un Plateau
  * @param p le Plateau
  **/
-string dessine(Plateau p);
+string dessine(Plateau p); /*implantée par Gloire LINVANI
 
 
-/** permet de savoir si une partie est termin�e
+/** permet de savoir si une partie est terminée
  *  @param plateau un Plateau
- *  @return true si le plateau est vide, false sinon
+ *  @return true si le plateau est terminée, false sinon
  **/
-bool estTermine(Plateau plateau);
+bool estTermine(Plateau plateau); /*implantée par Gloire LINVANI
 
 
-/** permet de savoir si une partie est gagn�e
+/** permet de savoir si une partie est gagnée
  * @param plateau un Plateau
  * @return true si le plateau contient un 2048, false sinon
  **/
-bool estGagnant(Plateau plateau);
+bool estGagnant(Plateau plateau); /*implantée par Gloire LINVANI
 
 
-//Custom fonctions
-
-
-/** retourner le score du plateau
- * @param plateau un Plateau
+/** renvoyer le score du plateau
+ * @param plat un Plateau
  * @return le score du plateau
  **/
-int score(Plateau plat);
+int score(Plateau plat); /*implantée par Huy NGUYEN
 
 
-/** permet de savoir si le dernier deplacement etait reussir
- * @param last le plateau avant le deplacement
- * @param plat le plateau apres le deplacement
- * @return true si le plateau ne deplace pas, false sinon
+/** permet de savoir si le dernier déplacement était réussi
+ * @param last le plateau avant le déplacement
+ * @param plat le plateau après le deplacement
+ * @return true si le plateau n'a pas été déplacé, false sinon
  **/
-bool nomoved(Plateau last, Plateau plat);
+bool notmoved(Plateau last, Plateau plat); /*implantée par Huy NGUYEN
 
 
-/** ajouter un nouveau bloc sur le plateau (2 ou 4). Si le jouer etait gagnent, ajoute des blocs plus grands.
+/** ajouter un nouveau bloc sur le plateau (2 ou 4). Si le jouer avait gagné, ajoute des blocs plus grands.
  * @param plateau un Plateau
- * @return le plateau apres on a ajoute un nouveau bloc
+ * @return le plateau après avoir ajouté un nouveau bloc
  **/
-Plateau addblock(Plateau plat);
+Plateau addblock(Plateau plat); /*implantée par Huy NGUYEN
 
 
-/** Calculer i pour que 2^i = c
- * @param c un integer
+/** Calculer i tel que 2^i = c
+ * @param c un entier
  * @return i
  **/
-int repow(int c);
+int repow(int c); /*implantée par Gloire LINVANI
 
-
-/** afficher le plateau en couleur
+/** afficher le plateau en couleurs
  * @param plat le Plateau
  **/
-void dessinecolor(Plateau plat);
+void dessinecolor(Plateau plat); /*implantée par Gloire LINVANI
 
 
-/** le code pour faire le jeux functionnel
+/** le code pour démarrer le jeu 
  **/
 void joue();
 #endif // MODELE_H_INCLUDED
